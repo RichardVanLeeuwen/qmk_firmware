1ab40e9390ff4224d70279c04b886086eb7db524
Richard van Leeuwen <rbavleeuwen@gmaile.com>
2025-09-04 20:12:39 +0200

    Made accidentally activating the gaming layer impossible

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/DrezaemRight/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/DrezaemRight/keymap.c
@@ -26,15 +26,18 @@
 #define _MOUSE 3
 #define _MEDIA 4
 #define _NUMBERS 5
-#define _SYMBOLS 6
-#define _FUNCTION 7
+#define _NUMPAD 6
+#define _SYMBOLS 7
+#define _FUNCTION 8
+#define _LAYER 9
 
-#define DVORAK DF(_DVORAK)
-#define GAME DF(_GAME)
+#define GAME TG(_GAME)
 #define NAVI LT(_NAVI, KC_ESC)
 #define MOUSPC LT(_MOUSE, KC_SPC)
 #define MEDIA LT(_MEDIA, KC_ENT)
 #define NUMBER LT(_NUMBERS, KC_TAB)
+#define NUMPAD LT(_NUMPAD, KC_ESC)
+#define LAYERS LT(_LAYER, KC_ESC)
 #define FUNCTIO LT(_FUNCTION, KC_DEL)
 #define SYMBOLS LT(_SYMBOLS, KC_BSPC)
 
@@ -47,19 +50,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LSFT,KC_SCLN, KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  , KC_W  , KC_V  , KC_Z  ,KC_EQL ,
                       KC_LEFT,KC_RGHT,                                                          KC_UP ,KC_DOWN,
                                       NAVI   ,MOUSPC ,                        SYMBOLS,NUMBER ,
-                                      GAME   ,MEDIA  ,                        FUNCTIO,KC_DEL ,
+                                      NUMPAD ,MEDIA  ,                        FUNCTIO,LAYERS ,
                                       KC_LSFT,KC_LCTL,                        KC_LGUI,KC_LALT
   ),
 
   [_GAME] = LAYOUT_5x6(
-      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSLS,
-      KC_TAB ,KC_QUOT,KC_COMM,KC_DOT , KC_P  , KC_Y  ,                         KC_F  , KC_G  , KC_C  , ALT_R , KC_L  ,KC_SLSH,
-      KC_CAPS,KC_A   ,KC_O   ,KC_E   , KC_U  , KC_I  ,                         KC_D  , KC_H  ,KC_T   , KC_N  , KC_S  ,KC_MINS,
-      KC_LSFT,KC_SCLN, KC_Q  , KC_J  , KC_K  , KC_X  ,                         KC_B  , KC_M  , KC_W  , KC_V  , KC_Z  ,KC_EQL ,
-                      KC_LEFT,KC_RGHT,                                                          KC_UP ,KC_DOWN,
-                                      NAVI   ,KC_SPC ,                        SYMBOLS,NUMBER ,
-                                      DVORAK ,MEDIA  ,                        FUNCTIO,KC_DEL ,
-                                      KC_LSFT,KC_LCTL,                        KC_LGUI,KC_LALT
+      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+      KC_CAPS, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+      KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,
+                      _______,_______,                                                        _______,_______,
+                                      KC_SPC ,KC_LCTL,                        KC_BSPC,KC_TAB ,
+                                      KC_ENT ,KC_DEL ,                        NUMPAD ,LAYERS ,
+                                      _______,GAME   ,                        _______,_______
   ),
 
   [_NAVI] = LAYOUT_5x6(
@@ -106,6 +109,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______,_______,                          _______,_______
   ),
 
+  [_NUMPAD] = LAYOUT_5x6(
+    _______,_______,_______,_______,_______,_______,                          _______,_______,_______,_______,_______,_______,
+    _______,_______,_______,_______,_______,_______,                          KC_PAST, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,_______,
+    _______,_______,_______,_______,_______,_______,                          KC_NUM , KC_P4 , KC_P5 , KC_P6 ,KC_MINS,_______,
+    QK_BOOT,_______,_______,_______,_______,_______,                          KC_P0  , KC_P1 , KC_P2 , KC_P3 ,KC_PEQL,QK_BOOT,
+                    _______,_______,                                                          _______,_______,
+                                    _______,_______,                          _______,_______,
+                                    _______,_______,                          _______,_______,
+                                    _______,_______,                          _______,_______
+  ),
+
   [_SYMBOLS] = LAYOUT_5x6(
       _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
       _______,_______,_______,_______,_______,_______,                        _______,KC_LCBR,KC_RCBR,KC_EQL ,KC_EXLM,_______,
@@ -128,6 +142,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______,_______,                          _______,_______
   ),
 
+ [_LAYER] = LAYOUT_5x6(
+      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,_______,_______,_______,_______,_______,                        _______,GAME   ,_______,_______,_______,_______,
+      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
+                      _______,_______,                                                        _______,_______,
+                                      _______,_______,                        _______,_______,
+                                      _______,_______,                        _______,_______,
+                                      _______,_______,                        _______,_______
+  ),
 };
 
 //   [_LAYER] = LAYOUT_5x6(
